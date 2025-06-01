b301eeb865bc0d030f3a4caedb3a13bebc639310
jailanmedhat <jailanmedhatt2@gmail.com>
2025-06-02 02:36:17 +0300

    team details ui

---
+++ SportsApp/teamDetails/view/CoachCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CoachCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Jailan Medhat on 01/06/2025.
+//
+
+import UIKit
+
+class CoachCollectionViewCell: UICollectionViewCell {
+    
+}
